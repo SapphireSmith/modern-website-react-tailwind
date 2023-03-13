2c38512f82f767b8f95ccdeface8e816868d577f
Sapphire Smith <ssapphire527@gmail.com>
2023-03-13 20:03:04 +0530

    Testimonial section completed

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
 const CardDetails = () => {
   return (
-    <div>CardDetails</div>
+    <section className={layout.sectionImg}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better
+          card dealer <br className='sm:block hidden' />
+          iin few easy steps. </h2>
+        <p className={`${styles.paragraph} max-w-[470px]
+          mt-5`}>So when the sm:block hidden classes
+          are used together, it means that the
+          element will be hidden by default on all
+          screen sizes, but will be displayed as a
+          block element on small screens. Here's an        
+        </p>
+        < Button style={'mt-10'} />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='
+        w-[100%] h-[100%]' />
+      </div>
+
+    </section>
   )
 }
 
