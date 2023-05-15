8701453b5e15be5573d1b17d3d8ad73d165ab5fd
Sapphire Smith <ssapphire527@gmail.com>
2023-05-15 15:21:45 +0530

    first commit

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Button from './Button'
+import styles from '../style'
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section className={`mt-14 ${styles.flexCenter} ${styles.marginY}
+  ${styles.padding} sm:flex-row flex-col
+  bg-black-gradient rounded-[20px] box-shadow`}>
+    <div className='flex-1 flex flex-col'>
+      <h2 className={`${styles.heading2}`}>Let's try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Every thing you need to accept card payments
+        and grow your business anywhere on the planet
+      </p>
+    </div>
+
+    <div className={`button ${styles.flexCenter} 
+    sm:ml-10 ml-0 sm:mt-6 mt-6`}>
+      <Button />
+    </div>
+  </section>
+)
+
 
 export default CTA
--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -5,11 +5,11 @@ import Button from './Button'
 
 const CardDetails = () => {
   return (
-    <section className={layout.sectionImg}>
-      <div className={layout.sectionInfo}>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo} >
         <h2 className={styles.heading2}>Find a better
           card dealer <br className='sm:block hidden' />
-          iin few easy steps. </h2>
+          in few easy steps. </h2>
         <p className={`${styles.paragraph} max-w-[470px]
           mt-5`}>So when the sm:block hidden classes
           are used together, it means that the
